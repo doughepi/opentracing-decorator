770814898e39d5243daef6af7d34fa5a8cda57e6
Piper Dougherty <doughertypiper@gmail.com>
2021-01-26 06:07:28 -0500

    Better imports.

--- opentracing_decorator/__init__.py
+++ opentracing_decorator/__init__.py
@@ -1,1 +1,9 @@
-from opentracing_decorator.tracing import Tracing
+from .__version__ import __title__, __description__, __version__
+from .tracing import Tracing
+
+__all__ = ["__title__", "__description__", "__version__", "Tracing"]
+
+__locals = locals()
+for __name in __all__:
+    if not __name.startswith("__"):
+        setattr(__locals[__name], "__module__", "opentracing_decorator")  # noqa
